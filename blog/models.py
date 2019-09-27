d9cee5cbe076a7f2bde07275d84919b57dc39377
Jerry Griffith <diggerofoz@gmail.com>
2019-09-27 02:46:11 -0700

    Ready to create a blog model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+# Create A Blog model.
+# title
+# pub_date
+# body
+# image
+
+
+# Add the Blog app to the settings
+
+# Create a migration
+
+# Migrate
+
+# Add to the admin
