cbd1badad911891f052749c48558f40972e280d9
Jerry Griffith <diggerofoz@gmail.com>
2019-09-28 02:51:06 -0700

    43-Blog detail complete

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,17 @@ class Blog(models.Model):
     body = models.TextField()
     image = models.ImageField(upload_to='images')
 
+    # The first def will change the admin page so instead of showing 'blog object' it will
+    # show the blog title.
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_summary(self):
+        return self.pub_date.strftime('%b %e %Y')
+
 # Create a migration
 
 # Migrate
