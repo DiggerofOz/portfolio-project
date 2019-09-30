3c049ca27a7175a9f4edd22aef71330287ca39fb
Griffith <griffj1@medtronic.com>
2019-09-29 21:24:25 -0700

    Github commit - no pass

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
         'USER':'postgres',
-        'PASSWORD':'0ff2C4me!pg',
+        'PASSWORD':'!pg',
         'HOST':'localhost',
         'PORT':'5432',
     }
