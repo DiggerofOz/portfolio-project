ebffea543047189c5b74981200b7ac257807c9fb
Griffith <griffj1@medtronic.com>
2019-09-29 23:05:07 -0700

    secure keys

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -133,3 +133,9 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = MEDIA_ROUTE = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+# added this so true keys are secure on the server
+try:
+    from local_settings import *
+except ImportError:
+    pass
